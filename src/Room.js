19031266ae4316e7c752902708247d361a7c6a64
git-bia19 <56919489+git-bia19@users.noreply.github.com>
2020-06-12 23:24:23 +0400

    created react-state app

---
+++ src/Room.js
@@ -0,0 +1,28 @@
+import React, {useState} from 'react';
+import './Room.css';
+
+
+function Room(){
+    let [isON, setON] = useState(false);
+    let [Temp,setTemp] = useState(22);
+   
+    return(
+         <div className= {`room ${isON ? "lit" : "dark" }`}>
+
+         This room is {!isON? "lit" : "dark"}
+         <br></br>
+         <button  onClick={()=>setON(!isON)}>ON </button>
+         <button  onClick={()=>setON(!isON)}>OFF</button>
+         <br></br> <br></br>
+         <div>
+         Current Temperature is: {Temp}
+         <br></br>
+         <button onClick={()=>setTemp(++Temp)}> + </button>
+         <button onClick={()=>setTemp(--Temp)}> - </button>
+         </div>
+        </div>
+        )
+
+}
+
+export default  Room;
